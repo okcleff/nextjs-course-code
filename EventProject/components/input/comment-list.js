7f414b65070a4f42e68dacad212901e311d4a141
okcleff <okcleff@gmail.com>
2023-05-25 12:30:19 +0900

    Event Project: adding API routes

--- EventProject/components/input/comment-list.js
+++ EventProject/components/input/comment-list.js
@@ -1,21 +1,22 @@
 import classes from './comment-list.module.css';
 
-function CommentList() {
+function CommentList(props) {
+  const { comments } = props;
+
   return (
     <ul className={classes.comments}>
-      {/* Render list of comments - fetched from API */}
-      <li>
-        <p>My comment is amazing!</p>
-        <div>
-          By <address>Maximilian</address>
-        </div>
-      </li>
-      <li>
-        <p>My comment is amazing!</p>
-        <div>
-          By <address>Maximilian</address>
-        </div>
-      </li>
+      {comments.map((comment) => {
+        const { id, name, text } = comment;
+
+        return (
+          <li key={id}>
+            <p>{text}</p>
+            <div>
+              By <address>{name}</address>
+            </div>
+          </li>
+        );
+      })}
     </ul>
   );
 }
